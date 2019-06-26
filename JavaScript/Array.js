aee279336e96d928d255488c1deff706790481d7
Denis <denchikiiii@gmail.com>
2019-06-26 23:32:52 +0700

    Иправлены ошибки в JavaScript Array: 1. Должна быть некоторая страница, в рамках которой выполняется этот скрипт
    2. Пока нельзя использовать let, он поддерживается не везде
    
    3. Имена функций должны быть с маленькой буквы
    
    4. Сортировку нужно использовать стандартную
    
    5. Нужно стараться избегать циклов по массиву.
    Вместо них нужно использовать filter/map/reduce/some/forEach и др.
    
    6. createArray - лучше вставлять в конец через push

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,37 +1,22 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-/**
- * Функция сортировки массива по убыванию
- * @param array Массив чисел
- * @constructor
- */
-function Sort(array) {
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-            if (array[i] < array[j]) {
-                let temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-            }
-        }
-    }
-}
+console.log("Начальный массив " + array);
 
-/**
- * Функция ссумирования четных чисел масива
- * @param array Массив чисел
- * @returns {number} Результат суммы
- * @constructor
- */
-function SumEvenValue(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (i % 2 === 1) {
-            sum += array[i];
-        }
-    }
-    return sum;
-}
+var sortArray = array.sort(function (a, b) {
+    return b - a;
+});
+
+console.log("Отсортированный массив по убыванию " + sortArray);
+
+var sumEvenValue = sortArray
+    .filter(function (item) {
+        return item % 2 === 1;
+    })
+    .reduce(function (sum, number) {
+        return sum + number;
+    });
+
+console.log("Сумма четных элементов массива равна " + sumEvenValue)
 
 /**
  * Создание массива от 1 до 100
@@ -39,42 +24,24 @@ function SumEvenValue(array) {
  */
 function createArray() {
     let array = [];
-    for(let i = 0; i < 100; i++) {
-        array[i] = i + 1;
+    for (let i = 0; i < 100; i++) {
+        array.push(i + 1);
     }
     return array;
 }
 
-/**
- * Получение массива квадратов четных чисел масисива
- * @param array Входной массив
- * @returns {Array} Массив квадратов четных чисел
- * @constructor
- */
-function SquareListEvenValue(array) {
-    let squareArray = [];
-    let j = 0;
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] % 2 === 0) {
-            squareArray[j] = Math.pow(array[i], 2);
-            j++;
-        }
+var squareListEvenValue = createArray().filter( function (item) {
+    if (item % 2 === 0) {
+        return Math.pow(item, 2);
     }
-    return squareArray;
-}
-
-console.log("Начальный массив " + array);
-Sort(array);
-console.log("Отсортированный массив по убыванию " + array);
+});
 
 let fistFiveValueArray = array.slice(0, 5);
 console.log("Первые пять чисел отсортированного массива " + fistFiveValueArray);
 
-let endFiveValueArray = array.slice(array.length - 5, array.length);
+let endFiveValueArray = array.slice(array.length - 5);
 console.log("Первые пять чисел отсортированного массива " + endFiveValueArray);
-
-console.log("Сумма четных элементов массива равна " + SumEvenValue(array))
 
 console.log("Массив от 1 до 100 " + createArray());
 
-console.log("Список квадратов четных чисел массива " + SquareListEvenValue(createArray()))
+console.log("Список квадратов четных чисел массива " + squareListEvenValue)
