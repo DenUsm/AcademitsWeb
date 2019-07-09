aa7d95f15dbca5adad546a521bc12b2eceab3f7e
Denis <denchikiiii@gmail.com>
2019-07-09 22:17:32 +0700

    Исправлены замечания: 1. Первый if обычно не нужен, можно всегда использовать DOMContentLoaded
    2. Лучше сделать переменные для DOM элементов.
    Так код будет читаться проще
    
    3. Весь интерфейс программы должен быть либо на русском, либо на английском
    
    4. Лучше сделать функции для перевода температур

--- JavaScript/ConvertTemperature/convertTemperature.js
+++ JavaScript/ConvertTemperature/convertTemperature.js
@@ -1,19 +1,36 @@
-if (document.readyState !== "loading") {
-    onReady();
-} else {
-    document.addEventListener("DOMContentLoaded", onReady);
-}
+document.addEventListener("DOMContentLoaded", onReady);
 
 function onReady() {
     document.getElementById("convert").addEventListener("click", function () {
-        var celsius = +document.getElementById("celsiusInput").value;
-        if (isNaN(celsius)) {
+        var celsius = document.getElementById("celsiusInput");
+        var fahrenheit = document.getElementById("fahrenheitOutput");
+        var kelvin = document.getElementById("kelvinOutput");
+
+        if (isNaN(+celsius.value)) {
             alert("Error. Please enter correct value")
         } else {
-            document.getElementById("fahrenheitOutput").innerHTML = (celsius * 1.8 + 32).toFixed(2);
-            document.getElementById("kelvinOutput").innerHTML = (celsius + 273.15).toFixed(2);
+            fahrenheit.innerHTML = celsius2Fahrenheit(celsius);
+            kelvin.innerHTML = celsius2Kelvin(celsius);
         }
     });
+}
+
+/**
+ * Перевод температуры цельсии в кельвины
+ * @param temp - температура цельсии
+ * @returns {string} - температура в кельвинах
+ */
+function celsius2Kelvin(temp) {
+    return (temp + 273.15).toFixed(2);
+}
+
+/**
+ * Перевод температуры цельсии в фаренгейты
+ * @param temp - температура цельсии
+ * @returns {string} - температура в фаренгейтах
+ */
+function celsius2Fahrenheit(temp) {
+    return (temp * 1.8 + 32).toFixed(2);
 }
 
 
