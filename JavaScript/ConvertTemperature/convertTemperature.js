350cf85d396863f48dd90fbf08e39a22497e7205
Denis <denchikiiii@gmail.com>
2019-07-14 23:43:33 +0700

    Исправлены замечания: 1. п.2 остался про onReady
    2. Сейчас не работает для числа 0.
    Это нормальное число, для него должно работать

--- JavaScript/ConvertTemperature/convertTemperature.js
+++ JavaScript/ConvertTemperature/convertTemperature.js
@@ -16,24 +16,29 @@ function celsiusToFahrenheit(temp) {
     return (temp * 1.8 + 32).toFixed(2);
 }
 
-document.addEventListener("DOMContentLoaded", onReady);
-
+/**
+ *  Функция обработки нажатия кнопки
+ */
 function onReady() {
     document.getElementById("convert").addEventListener("click", function () {
         var celsius = document.getElementById("celsiusInput");
         var fahrenheit = document.getElementById("fahrenheitOutput");
         var kelvin = document.getElementById("kelvinOutput");
 
-        var celsiusValue = +celsius.value;
+        var celsiusValue = celsius.value;
 
-        if (isNaN(celsiusValue) || (celsiusValue === 0)) {
+        if (isNaN(+celsiusValue) || (celsiusValue === "")) {
             alert("Ошибка! Введите корректное значение");
         } else {
-            fahrenheit.innerHTML = celsiusToFahrenheit(celsiusValue);
-            kelvin.innerHTML = celsiusToKelvin(celsiusValue);
+            fahrenheit.innerHTML = celsiusToFahrenheit(+celsiusValue);
+            kelvin.innerHTML = celsiusToKelvin(+celsiusValue);
         }
     });
 }
+
+document.addEventListener("DOMContentLoaded", onReady);
+
+
 
 
 
